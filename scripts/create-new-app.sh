135540d7ad29313857aa0af2373a00f96d27f76b
Patrick Pircher <patrick.pircher@ibm.com>
2025-12-03 20:10:11 +0100

    more compat

--- scripts/create-new-app.sh
+++ scripts/create-new-app.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-npx ember-cli new "$@" --typescript
+
+pnpx ember-cli new "$@" --typescript
 cd $1
 
-curl https://raw.githubusercontent.com/patricklx/ember-tui/refs/heads/scripts/scripts/setup.sh | bash "$@"
+curl https://raw.githubusercontent.com/patricklx/ember-tui/refs/heads/scripts/scripts/setup.sh | bash -s -- "$@"
